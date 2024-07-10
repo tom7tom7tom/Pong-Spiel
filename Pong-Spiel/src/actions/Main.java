7a5e0d6adfb3c81cb514439d38e79e55315a9143
tom7tom7tom <mathiaskagu@gmail.com>
2024-07-10 08:00:30 +0200

    Add files via upload

---
+++ Pong-Spiel/src/actions/Main.java
@@ -0,0 +1,22 @@
+package actions;
+
+import org.w3c.dom.Text;
+
+import game.GameLogic;
+import gui.Gui;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+
+		GameLogic spiellogik = new GameLogic();
+		new Gui(spiellogik);
+		
+		
+	
+	
+	
+	
+}
+}
